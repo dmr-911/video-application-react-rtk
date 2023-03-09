eeb3a66a50a5fcbdcfec2804930d2ff4683415e9
dmr-911 <dewan.mizanur911@gmail.com>
2023-03-10 00:24:37 +0600

    filter slice created and filter and search section created ahead of time

--- src/app/store.js
+++ src/app/store.js
@@ -3,6 +3,7 @@ import videosReducer from "../features/videos/videosSlice";
 import tagsReducer from "../features/tags/tagsSlice";
 import videoReducer from "../features/video/videoSlice";
 import relatedVideosReducer from "../features/relatedVideos/relatedVideosSlice";
+import filterReducer from "../features/filter/filterSlice";
 
 export const store = configureStore({
   reducer: {
@@ -10,6 +11,7 @@ export const store = configureStore({
     tags: tagsReducer,
     video: videoReducer,
     relatedVideos: relatedVideosReducer,
+    filter: filterReducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(),
 });
